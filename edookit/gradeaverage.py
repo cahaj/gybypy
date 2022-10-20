ae841ba7c44f26d33e061b479e956cd474ad7f83
Jiri <63856816+cahaj@users.noreply.github.com>
2022-10-20 19:04:52 +0200

    Patches

--- edookit/gradeaverage.py
+++ edookit/gradeaverage.py
@@ -15,13 +15,17 @@ def gradeAverage(access1, access2):
         amount = 0
         for markDict in markList:
             for mark, wage in markDict.items():
-                if wage == '0.5':
-                    wage = 1
-                else:
-                    wage = int(wage) * 2
-                wagedMark = int(mark) * wage
-                total = total + wagedMark
-                amount = amount + wage
-        avrg.append({subj: total / amount})
+                if mark not in ["Splnil", "Nesplnil", "X"]:
+                    if wage == '0.5':
+                        wage = 1
+                    else:
+                        wage = int(wage) * 2
+                    wagedMark = int(mark) * wage
+                    total = total + wagedMark
+                    amount = amount + wage
+        if amount != 0:
+            avrg.append({subj: round(total / amount, 1)})
+        else:
+            pass
     
     return avrg
--- edookit/scraper.py
+++ edookit/scraper.py
@@ -31,9 +31,13 @@ class Scraper():
         self.actions = ActionChains(self.driver)
 
     def login(self):
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'plus4ULoginButton'))).click()
         time.sleep(1)
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'plus4ULoginButton'))).click()
+        time.sleep(1)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'plus4ULoginButton'))).click()
+        except:
+            pass
 
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'accessCode1'))).click()
         self.actions.send_keys(self.access1).perform()
@@ -41,8 +45,7 @@ class Scraper():
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'accessCode2'))).click()
         self.actions.send_keys(self.access2).perform()
 
-        element = self.driver.find_element(By.XPATH, "/html/body/div/div/div[4]/div/div[1]/div/div/div[2]/div/div[3]/div/div[1]/form/button[1]")
-        element.click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[4]/div/div[1]/div/div/div[2]/div/div[4]/div/div[1]/form/button[1]"))).click()
 
         print(colored("scraper.py:", "cyan"), "LOGIN")
 
