ae4e634aa2a12057813938e6ed53f059ef9bb78c
Jiri <63856816+cahaj@users.noreply.github.com>
2022-10-22 13:28:37 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -8,11 +8,24 @@ from discord import CategoryChannel, option
 from discord.ext import commands, tasks
 import datetime
 import time
+import requests
 
 from edookit.scraper import Scraper
 import outlook.calendar as cal
 import edookit.gradeaverage as ga
 
+def inbotdir():
+    dir = f"{os.getcwd()}\\bot"
+    indir = os.listdir(dir)
+    indir.remove("__pycache__")
+    return indir
+
+def writeable():
+    list = inbotdir()
+    for c, i in enumerate(list):
+        if ".py" in i:
+            del list[c]
+    return list
 
 bot = commands.Bot(command_prefix = ':', activity=discord.Activity(type=discord.ActivityType.watching, name="/help"))
 bot.remove_command('help')
@@ -41,9 +54,8 @@ async def help(ctx: discord.ApplicationContext):
 @option(
     name="access2",description="Access Code 2",required=True, option_type=3
 )
-async def help(ctx: discord.ApplicationContext, access1: str, access2: str):
+async def avrg(ctx: discord.ApplicationContext, access1: str, access2: str):
     embed = discord.Embed(title=f"Průměr známek",timestamp=datetime.datetime.utcnow(), description="Scraping and calculating all the data...")
-    embed.set_author(name=f"made by azurim#8202", url=f"https://www.instagram.com/mr_rohland/")
     await ctx.respond(embed=embed)
 
     embed = discord.Embed(title=f"Průměr známek",timestamp=datetime.datetime.utcnow(), description=f"Data for: {ctx.author}")
@@ -68,6 +80,58 @@ async def help(ctx: discord.ApplicationContext, access1: str, access2: str):
 
     embed.add_field(name="Time taken", value=f"```{elapsed_time}```", inline="False")
     await ctx.edit(embed=embed)
+
+
+@bot.slash_command(
+    name="download",
+    description="Download",
+)
+@option(
+    name="data",description="Data to download",required=True, option_type=3, choices = inbotdir()
+)
+@option(
+    name="hidden",description="Hidden?",required=True, option_type=3, choices=["True", "False"]
+)
+async def download(ctx: discord.ApplicationContext, data: str, hidden: str):
+    if hidden == "True":
+        hide = True
+    else:
+        hide = False
+    if ctx.author.id == 673799940560125952:
+        try:
+            file = data
+            await ctx.respond(file=discord.File(f"bot/{file}"), ephemeral=hide)
+        except Exception as e:
+            print(e)
+            await ctx.respond("Invalid file", ephemeral=hide)
+    else:
+        await ctx.respond("No permissions", ephemeral=True)
+
+@bot.slash_command(name="write")
+@option(
+    "file",
+    discord.Attachment,
+    description="File",
+)
+@option(
+    name="to",description="File",required=True, choices = writeable()
+)
+async def say(
+    ctx: discord.ApplicationContext, file: discord.Attachment, to: str):
+    if ctx.author.id == 673799940560125952:
+        try:
+            writeto=to
+            r = requests.get(file.url)
+            data = r.json()
+            print(data)
+            with open(writeto, "w") as obj:
+                json.dump(data, obj)
+            await ctx.respond(f"Written {file.url} to {writeto}")
+        except Exception as e:
+            print(e)
+            await ctx.respond("Exception occured")
+    else:
+        ctx.respond("No permissions", ephemeral=True)
 
 def runbot(token: str):
     bot.run(token)
