d9704fca6c866680f995ec93749d29c287de24ed
Jiri <63856816+cahaj@users.noreply.github.com>
2022-11-01 11:33:57 +0100

    Update keep_alive.py

--- bot/keep_alive.py
+++ bot/keep_alive.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from threading import Thread
+import random
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is online"
+    return "Bot online."
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=random.randint(1000, 10000))
 
 def keep_alive():  
     t = Thread(target=run)
