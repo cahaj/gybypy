1d7d99944fbc470954ac26b2a0585b4b3eecdcd9
Jiri <63856816+cahaj@users.noreply.github.com>
2022-11-01 11:35:10 +0100

    Update runbot.py

--- runbot.py
+++ runbot.py
@@ -1,4 +1,13 @@
 import bot.bot as bot
 from bot.keep_alive import keep_alive
-keep_alive()
-bot.runbot("TOKEN")
+import discord
+import os
+
+while __name__ == '__main__':
+  try:
+    keep_alive()
+    bot.runbot(os.environ['token'])
+  except discord.errors.HTTPException as e:
+    print(e)
+    print("\n\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
+    os.system('kill 1')
